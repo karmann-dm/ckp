76e81faba58a66749358e95edca4d416c3f29862
Karmanov Dmitry <karmanno@gmail.com>
2018-06-29 15:31:54 +1000

    Initial commit.

---
+++ src/main/java/org/ccfebras/ckp/CkpApplication.java
@@ -0,0 +1,12 @@
+package org.ccfebras.ckp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkpApplication.class, args);
+    }
+}
