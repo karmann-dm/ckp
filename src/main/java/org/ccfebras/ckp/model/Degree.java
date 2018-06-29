25a71beeb8d56528cd6a5760bc399ee086c7f900
Karmanov Dmitry <karmanno@gmail.com>
2018-06-29 16:17:15 +1000

    Filled degree model.

--- src/main/java/org/ccfebras/ckp/model/Degree.java
+++ src/main/java/org/ccfebras/ckp/model/Degree.java
@@ -1,4 +1,27 @@
 package org.ccfebras.ckp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "degrees", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
+@NoArgsConstructor
+@AllArgsConstructor
 public class Degree {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column(name = "name")
+    @Getter
+    @Setter
+    private String name;
 }
