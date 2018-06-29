670b268cf4802cb8ce4d6309f7294a78b15272bc
Karmanov Dmitry <karmanno@gmail.com>
2018-06-29 16:18:40 +1000

    Filled rank model.

--- src/main/java/org/ccfebras/ckp/model/Rank.java
+++ src/main/java/org/ccfebras/ckp/model/Rank.java
@@ -1,4 +1,27 @@
 package org.ccfebras.ckp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ranks", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rank {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column(name = "name")
+    @Getter
+    @Setter
+    private String name;
 }
