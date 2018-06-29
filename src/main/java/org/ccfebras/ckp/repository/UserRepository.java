606b2ee8869c4d9ecafebfe1d75a92d72b63a9ae
Karmanov Dmitry <karmanno@gmail.com>
2018-06-29 16:40:38 +1000

    Created user JPA repository.

---
+++ src/main/java/org/ccfebras/ckp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.ccfebras.ckp.repository;
+
+import org.ccfebras.ckp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findByIdIn(List<Long> userIds);
+    Boolean existsByEmail(String email);
+}
