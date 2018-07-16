1703e3ab135e6c8a6350a52d0993b865dc566e04
Karmanov Dmitry <karmanno@gmail.com>
2018-07-16 16:08:05 +1000

    Created exception to handle resource not found error.

---
+++ src/main/java/org/ccfebras/ckp/exception/NotFoundException.java
@@ -0,0 +1,23 @@
+package org.ccfebras.ckp.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@Getter
+@Setter
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String resourceName, String field, Object value) {
+        super(String.format("%s with %s = %s not found", resourceName, field, value));
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
