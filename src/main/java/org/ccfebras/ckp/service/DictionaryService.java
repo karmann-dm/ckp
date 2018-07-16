3254e993415408673fadc99f45cb18dfc908e37b
Karmanov Dmitry <karmanno@gmail.com>
2018-07-16 16:13:20 +1000

    Removed existing checkers.

--- src/main/java/org/ccfebras/ckp/service/DictionaryService.java
+++ src/main/java/org/ccfebras/ckp/service/DictionaryService.java
@@ -1,18 +1,16 @@
 package org.ccfebras.ckp.service;
 
-import org.ccfebras.ckp.model.Degree;
-import org.ccfebras.ckp.model.Organization;
-import org.ccfebras.ckp.model.Position;
-import org.ccfebras.ckp.model.Rank;
+import org.ccfebras.ckp.model.*;
 
 public interface DictionaryService {
-    boolean organizationExistsById(Long id);
-    boolean positionExistsById(Long id);
-    boolean degreeExistsById(Long id);
-    boolean rankExistsById(Long id);
-
     Organization findOrganizationById(Long id);
+    Organization saveOrganization(Organization organization);
+
     Position findPositionById(Long id);
-    Rank findRankById(Long id);
+
     Degree findDegreeById(Long id);
+
+    Rank findRankById(Long id);
+
+    Department findDepartmentById(Long id);
 }
--- src/main/java/org/ccfebras/ckp/service/DictionaryServiceImpl.java
+++ src/main/java/org/ccfebras/ckp/service/DictionaryServiceImpl.java
@@ -1,67 +1,71 @@
 package org.ccfebras.ckp.service;
 
-import org.ccfebras.ckp.model.Degree;
-import org.ccfebras.ckp.model.Organization;
-import org.ccfebras.ckp.model.Position;
-import org.ccfebras.ckp.model.Rank;
-import org.ccfebras.ckp.repository.DegreeRepository;
-import org.ccfebras.ckp.repository.OrganizationRepository;
-import org.ccfebras.ckp.repository.PositionRepository;
-import org.ccfebras.ckp.repository.RankRepository;
+import org.ccfebras.ckp.exception.NotFoundException;
+import org.ccfebras.ckp.exception.ResourceNotFoundException;
+import org.ccfebras.ckp.model.*;
+import org.ccfebras.ckp.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DictionaryServiceImpl implements DictionaryService {
-    @Autowired
-    private OrganizationRepository organizationRepository;
+    private final OrganizationRepository organizationRepository;
+    private final PositionRepository positionRepository;
+    private final RankRepository rankRepository;
+    private final DegreeRepository degreeRepository;
+    private final DepartmentRepository departmentRepository;
 
-    @Autowired
-    private PositionRepository positionRepository;
-
-    @Autowired
-    private RankRepository rankRepository;
-
-    @Autowired
-    private DegreeRepository degreeRepository;
-
-    @Override
-    public boolean organizationExistsById(Long id) {
-        return organizationRepository.existsById(id);
+    public DictionaryServiceImpl(
+            OrganizationRepository organizationRepository,
+            PositionRepository positionRepository,
+            RankRepository rankRepository,
+            DegreeRepository degreeRepository,
+            DepartmentRepository departmentRepository
+    ) {
+        this.organizationRepository = organizationRepository;
+        this.positionRepository = positionRepository;
+        this.rankRepository = rankRepository;
+        this.degreeRepository = degreeRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
-    public boolean positionExistsById(Long id) {
-        return positionRepository.existsById(id);
-    }
-
-    @Override
-    public boolean degreeExistsById(Long id) {
-        return degreeRepository.existsById(id);
-    }
-
-    @Override
-    public boolean rankExistsById(Long id) {
-        return rankRepository.existsById(id);
+    public Organization saveOrganization(Organization organization) {
+        return organizationRepository.save(organization);
     }
 
     @Override
     public Organization findOrganizationById(Long id) {
-        return organizationRepository.findById(id).orElse(null);
+        return organizationRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Organization", "id", id));
     }
 
     @Override
     public Position findPositionById(Long id) {
-        return positionRepository.findById(id).orElse(null);
+        return positionRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Position", "id", id));
     }
 
     @Override
     public Rank findRankById(Long id) {
-        return rankRepository.findById(id).orElse(null);
+        return rankRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Rank", "id", id));
     }
 
     @Override
     public Degree findDegreeById(Long id) {
-        return degreeRepository.findById(id).orElse(null);
+        return degreeRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Degree", "id", id));
+    }
+
+    @Override
+    public Department findDepartmentById(Long id) {
+        return departmentRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Department", "id", id));
     }
 }
